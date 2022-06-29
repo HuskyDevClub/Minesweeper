606b2d9ce138d793d22ebd2e81f68695288e6fb4
AnyLeftovers <37920527+yudonglin@users.noreply.github.com>
2022-06-28 20:40:31 -0700

    add mouldes to generate hint for mine field

--- GriffinMinesweeper/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-
-  public static void main(String[] args){
-
-    // this is just to upload my project directory to the
-    // git repository. Fake Java file right here.
-  }
-}
---
+++ GriffinMinesweeper/src/main.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class main {
+    public static void main(String[] args) throws IOException {
+        final var _lines = (new BufferedReader(new FileReader(("minesweeper_input.txt")))).lines().toList();
+        int index = 0;
+        final var output = new StringBuilder();
+        int totalMineFields = 1;
+
+        while (index < _lines.size()) {
+            final var mineFieldInfo = _lines.get(index).split("\\s+");
+            final var _row = Integer.parseInt(mineFieldInfo[0]);
+            final var _column = Integer.parseInt(mineFieldInfo[1]);
+            if (_row > 0) {
+                output.append("Field #");
+                output.append(totalMineFields);
+                totalMineFields++;
+                output.append(":\n");
+                final var mineField = new char[_row][_column];
+                for (int i = 0; i < _row; i++) {
+                    mineField[i] = _lines.get(index + 1 + i).toCharArray();
+                }
+                output.append((new Field(mineField)).getHint());
+                index += _row + 1;
+                output.append("\n");
+            } else {
+                break;
+            }
+        }
+
+        Files.writeString(Paths.get("output.txt"), output.toString(), StandardCharsets.UTF_8);
+    }
+}
