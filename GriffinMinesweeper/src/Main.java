4c62823177788da584b37a388deeaeab21175895
griffinryan <glryan@uw.edu>
2022-06-29 10:27:04 -0700

    began my individual solution

---
+++ GriffinMinesweeper/src/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Main.java
+ * Griffin Ryan
+ */
+import java.util.Scanner;
+/**
+ * Main is the exectuable for the Minesweeper
+ * project. 
+ * 
+ * Running this will take an input .txt file as a 
+ * command-line argument and output to another .txt 
+ * file that is given as a second command-line argument.
+ * 
+ * @author Griffin Ryan (glryan@uw.edu)
+ * @version 0.0.1
+ */
+public class Main {
+
+}
