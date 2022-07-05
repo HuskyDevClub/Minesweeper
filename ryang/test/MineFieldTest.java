0e8afac4c169cfa8879d0db8c7101f17410e6ad7
griffinryan <glryan@uw.edu>
2022-07-05 14:02:56 -0700

    started JUnit integration

---
+++ ryang/test/MineFieldTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MineFieldTest {
+
+	@Test
+	void getRow() {
+	}
+
+	@Test
+	void getColumn() {
+	}
+
+	@Test
+	void getHint() {
+	}
+}
