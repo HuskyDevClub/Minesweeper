d6ff8345caaa6f265091bcf4f942f20d03061b0e
griffinryan <glryan@uw.edu>
2022-07-06 12:47:51 -0700

    fixes

--- ryang/src/MineField.java
+++ ryang/src/MineField.java
@@ -6,7 +6,7 @@
  * MineField is an Object for Minesweeper to use as a
  * game board in a session.
  *
- * Instiating a MineField object will fill a 2D char array's
+ * Instantiating a MineField object will fill a 2D char array's
  * 'safe' spots (. not *) with hint/number indicators.
  *
  * @author Griffin Ryan (glryan@uw.edu)
